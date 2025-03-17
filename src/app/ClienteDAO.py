252a7c9c6635a38408a72dcb54725128a841d5f1
Bernardo <luckmanbernardo@gmail.com>
2025-03-17 18:50:00 -0300

    segurança

--- src/app/ClienteDAO.py
+++ src/app/ClienteDAO.py
@@ -2,15 +2,22 @@ import db
 from fastapi import APIRouter
 from domain.entities.Cliente import Cliente
 from infra.orm.ClienteModel import ClienteDB
+from typing import Annotated
+from fastapi import Depends
+from security import get_current_active_user, User
 
-router = APIRouter()
+router = APIRouter( dependencies=[Depends(get_current_active_user)] )
 
-@router.get("/cliente/", tags=["Cliente"])
-async def get_cliente():
+
+@router.get("/cliente/", tags=["Cliente"], dependencies=[Depends(get_current_active_user)],)
+async def get_cliente( current_user:Annotated[User, Depends(get_current_active_user)], ):
     try:
         session = db.Session()
        
         dados = session.query(ClienteDB).all()
+
+        print(current_user)
+
         return dados, 200
     
     except Exception as e:
